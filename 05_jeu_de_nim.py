d824d13609034a64668a3eb8e51c9402f0c03372
Michaël Seyne <michaelseyne@gmail.com>
2015-10-03 21:02:09 +0200

    jeu de nim

--- 05_jeu_de_nim.py
+++ 05_jeu_de_nim.py
@@ -1,6 +1,59 @@
 # -*- coding:Utf-8 -*-
-#jeu de nim
-#http://www.atariarchives.org/basicgames/showpage.php?page=118
-#https://en.wikipedia.org/wiki/Nim
-#https://fr.wikipedia.org/wiki/Jeux_de_Nim
-#https://www.youtube.com/watch?v=NqsZ8DD6WHU
+'''jeu de nim; 
+https://fr.wikipedia.org/wiki/Jeux_de_Nim
+deux variantes :
+- une seule ligne de baton, le gagnant est celui qui prend le dernier baton, celui qui est le dernier à jouer, gagne.
+- cinqs lignes de batons de quantités différentes (1, 3, 5, 7), on prend sur une ligne autant de baton que l'on souhaite
+celui qui prend le dernier baton à perdu.
+'''
+
+BATON = ('''
+  _ 
+ | | 
+ | | 
+ |_| 
+
+ ''')
+
+BATON2 = {'a' : ' _ ', 'b' : '| | ', 'c' : '|_| '}
+
+NOMBRE = 20
+
+PYRAMIDE = {"1" : 1, "2" : 3, "3" : 5, "4" : 7}
+
+def jeu_pyramide():
+	print("pyramide")
+	pass
+
+def jeu_ligne():
+	print("ligne")
+	pass
+
+
+def afficher_baton():
+	x = BATON
+	print(BATON2['a'])
+	print(BATON2['b'])
+	print(BATON2['b'])
+	print(BATON2['c'])
+	#for i in range(NOMBRE):
+	#	print(BATON)
+
+def menu():
+	print("Jeu de Nim")
+	print("Quelle variante du jeu souhaitez vous jouer ?")
+	print("1) Batons en ligne (nim)")
+	print("2) Batons en pyramide (marienbad)")
+	answer = input(">>>")
+	while answer != "1" and answer != "2" and answer != "q":
+		answer = input("Variante 1 ou 2 ?")
+	if answer == "1":
+		jeu_ligne()
+	elif answer == "2":
+		jeu_pyramide()
+	else:
+		print("Bye")
+
+if __name__ == "__main__":
+	#menu()
+	afficher_baton()
