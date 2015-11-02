61cbb5739f3ad3de98a0a375721eb46eadbde4c6
Michaël Seyne <michaelseyne@gmail.com>
2015-11-02 09:18:25 +0100

    + dev 06

--- 06_morpion.py
+++ 06_morpion.py
@@ -1,11 +1,6 @@
 # -*- coding:Utf-8 -*-
 "le jeu du morpion/Tic Tac Toe"
 
-'''
-- un systeme de reset du tableau pour remettre tout à zéro quand une nouvelle partie
-- le tour de l'ordinateur et une petit IA
-'''
-
 #bibliothèques importé (random sera utilisé pour l'IA)
 import random
 import sys
@@ -14,10 +9,12 @@ import time
 
 # variables
 
+# définition des noms des RANGS et des COLONNES
 RANG = ["r1", "r2", "r3"]
-CASE = ["c1", "c2", "c3"]
+COLN = ["c1", "c2", "c3"]
 
-COORDINATES = {
+# traduction des coordonnées visuelles CASE en coordonées RANG/COLONNES
+CASES = {
 "a1" : ["r1", "c1"],
 "a2" : ["r2", "c1"],
 "a3" : ["r3", "c1"],
@@ -29,12 +26,14 @@ COORDINATES = {
 "c3" : ["r3", "c3"],
 }
 
+# disposition du plateau de jeu
 TABLEAU = { 
 "r1" : {"c1":0, "c2":0, "c3":0},
 "r2" : {"c1":0, "c2":0, "c3":0},
 "r3" : {"c1":0, "c2":0, "c3":0},
 }
 
+# définition de toutes les victoires possibles
 VICTOIRES = [
 	["a1", "b2", "c3"],
 	["a3", "b2", "c1"],
@@ -64,13 +63,15 @@ JEU = [
 "Cette case à déjà été choisi, choisissez en une autre s'il vous plaît.",
 "Bravo vous avez gagné ! :)",
 "Dommage vous avez perdu ! :(",
-"C'est au tour de l'ordinateur de jouer."
+"C'est au tour de l'ordinateur de jouer.",
+"L'ordinateur choisi de jouer"
 ]
 
 #fonctions
 
 def afficherTableau(tableau):
 	"une grille de 7x6"
+	os.system('cls')
 	c = 1
 	print("\n     a   b   c")
 	print("")
@@ -78,7 +79,7 @@ def afficherTableau(tableau):
 		print("%s  | " % c, end="")
 		c += 1
 		# print("\nligne :", i)
-		for x in CASE:
+		for x in COLN:
 			# print("case", x, ":", end=" ")
 			if tableau[i][x] == 0:
 				print(".", "|", end=" ")
@@ -111,7 +112,7 @@ def verifInput():
 	print(JEU[0])
 	print(JEU[1])
 	while True:
-		answer = inputPrompt()
+		answer = inputPrompt().lower()
 		
 		if len(answer) == 2:
 			answer = reverseText(answer)
@@ -127,6 +128,7 @@ def verifInput():
 
 
 def reverseText(string):
+	"retourne une position valide en renversant une valeur qui serait donnée tel que 3a ou c1"
 	newString = ""
 	if string[0] in list("123"):
 		newString += string[1]
@@ -141,7 +143,7 @@ def reverseText(string):
 
 def verifTableau(chx, tb):
 	"vérifie si la case n'a pas déjà été choisi, return false si elle est prise, true si elle est libre"
-	coords = COORDINATES[chx]
+	coords = CASES[chx]
 	case = tb[coords[0]][coords[1]]
 	if case == 0:
 		return True
@@ -151,21 +153,27 @@ def verifTableau(chx, tb):
 
 def updateTableau(chx, tr, tb):
 	"met à jour le tableau en fonction du choix envoyé"
-	coords = COORDINATES[chx] #traduit le choix en coordonnées dans le tableau
+	coords = CASES[chx] #traduit le choix en coordonnées dans le tableau
 	if tr == 1:
 		tb[coords[0]][coords[1]] = 1
 	elif tr == 2:
 		tb[coords[0]][coords[1]] = 2
 	return tb
 
+def nettoyerTableau(tb):
+	"remet toutes les cases à 0"
+	for i in RANG:
+		for c in COLN:
+			tb[i][c] = 0
+
 def verifVictoire(tab, tr):
 	"retourne true si une ligne du tableau est complète"
 	compteur = 0
 
 	for i in VICTOIRES:
-		xy1 = COORDINATES[i[0]]
-		xy2 = COORDINATES[i[1]]
-		xy3 = COORDINATES[i[2]]
+		xy1 = CASES[i[0]]
+		xy2 = CASES[i[1]]
+		xy3 = CASES[i[2]]
 		c1 = tab[xy1[0]][xy1[1]]
 		c2 = tab[xy2[0]][xy2[1]]
 		c3 = tab[xy3[0]][xy3[1]]
@@ -175,26 +183,92 @@ def verifVictoire(tab, tr):
 
 	# print("debug02, compteur =", compteur)		
 	if compteur == 1:
-		print("YES !")
 		return True
 	else:
 		return False
 
+def tourOrdinateur(tab):
+	"vérifie le tableau et les victoires possibles et choisit en conséquence"
+	
+	casesTop = []
+	index = 0
+
+	for i in VICTOIRES:
+		compteur = 0 
+
+		#les coordonées de chaque cases
+		xy1 = CASES[i[0]]
+		xy2 = CASES[i[1]]
+		xy3 = CASES[i[2]]
+		
+		#le contenu de chaque cases :
+		c1 = tab[xy1[0]][xy1[1]]
+		c2 = tab[xy2[0]][xy2[1]]
+		c3 = tab[xy3[0]][xy3[1]]
+
+		#vérifie chaque case et signale le compteur si elle sont occupé
+		if c1 > 0:
+			compteur += 1
+		if c2 > 0:
+			compteur += 1
+		if c3 > 0:
+			compteur += 1
+
+		if compteur == 2:
+			casesTop.append({i[0]:"", i[1]:"", i[2]:"", "flag":0})
+			casesTop[index][i[0]] = c1
+			casesTop[index][i[1]] = c2
+			casesTop[index][i[2]] = c3
+			index += 1
+
+	if len(casesTop) > 0:
+		print(casesTop) #DEBUG
+		#écrire le choix de l'ordinateur en fonction de ce qui se présente ici :
+		choice = None
+		flag1, flag2 = 0, 0
+
+		for i in casesTop:
+			print(i)
+			for c in i:
+				print(c, "case :", i[c])
+				if i[c] == 1:
+					flag1 += 1
+				if i[c] == 2:
+					flag2 += 1
+			
+			if flag1 >= 2:
+				i["flag"] = 1
+			elif flag2 >= 2:
+				i["flag"] = 2
+		
+		print(casesTop)
+		input() #DEBUG
+
+		if choice is not None:
+			return choice
+		else:
+			return random.choice(list(CASES))
+
+
+
+	else:
+		return random.choice(list(CASES))
+
+
 
 def programme():
 	"boucle principale, affichage, input et vérification"
-	# instance du tableau
-	tableau = TABLEAU.copy()
-	game = 1
+	# variable référence du tableau TABLEAU
+	tableau = TABLEAU
+	nettoyerTableau(tableau) # nettoyage à zéro pour une nouvelle partie
+	afficherTableau(tableau) # un premier affichage avant la boucle
 
-	while game:
-		# affichage jeu
-		os.system('cls')
-		afficherTableau(tableau)
+	while True:
 
 		# tour du joueur
 		tour = 1
 		chxJoueur = verifInput()
+
 		while True:
 			if verifTableau(chxJoueur, tableau):
 				tableau = updateTableau(chxJoueur, tour, tableau)
@@ -202,21 +276,41 @@ def programme():
 			else:
 				print("\n", JEU[5], "\n")
 				chxJoueur = verifInput()
+		
+		# affichage du jeu pour tour 1
+		afficherTableau(tableau)
 
 		# vérification victoire
 		if verifVictoire(tableau, tour):
 			print(JEU[6])
 			time.sleep(2)
-			game = 0
+			break
 
 		# tour de l'ordinateur
-		# tour = 2
-		# print(JEU[8])
-		# time.sleep(1) #debug
+		tour = 2
+		print(JEU[8])
+		time.sleep(1)
+
+		chxOrdi = tourOrdinateur(tableau)
+
+		while True:
+			print("debug:", chxOrdi)
+			if verifTableau(chxOrdi, tableau):
+				tableau = updateTableau(chxOrdi, tour, tableau)
+				break
+			else:
+				chxOrdi = tourOrdinateur(tableau)
+
+		print(JEU[9], chxOrdi, end=".\n")
+
+		# affichage jeu du tour 2
+		afficherTableau(tableau)
 
 		# vérification victoire
-		# if verifVictoire(tableau, tour):
-		# 	print(JEU[7])
+		if verifVictoire(tableau, tour):
+			print(JEU[7])
+			time.sleep(2)
+			break
 
 
 def menu():
