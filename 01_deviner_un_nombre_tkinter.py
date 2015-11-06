837464d813f30fe28b02a01a599da5179783d5ef
Michaël Seyne <michaelseyne@gmail.com>
2015-11-06 12:31:02 +0100

    + dev 01 dev nb tk

--- 01_deviner_un_nombre_tkinter.py
+++ 01_deviner_un_nombre_tkinter.py
@@ -12,22 +12,30 @@
 from tkinter import *
 import sys
 from random import randint
+import tkinter.messagebox
+import time
 
 #variables
 
 nbChoix = 0
 nbTours = 6
 nbEssai = []
+listNb = ""
 
 #fonctions
 
 def start():
 	"Remise à zéro pour une nouvelle partie"
-	global nbTours, nbChoix, nbEssai
-	can1.itemconfig(txt1, text="Essayez de deviner le nombre !")
+	global nbTours, nbChoix, nbEssai, listNb
 	nbTours = 6
 	nbChoix = randint(0, 100)
 	nbEssai = []
+	listNb = ""
+	can1.itemconfig(txt1, text="Essayez de deviner le nombre !")
+	can1.itemconfig(recJeu, fill="light grey")
+	can1.itemconfig(txtJeu, text="")
+	can1.itemconfig(txtEssai, text="")
+	can1.itemconfig(txtTour, text="Nombre de tours restants : {}".format(nbTours))
 
 def exit():
 	sys.exit()
@@ -35,11 +43,11 @@ def exit():
 def nouvellePartie():
 	"demande si l'on souhaite faire une nouvelle partie"
 
-	#efface l'entree et le bouton ok pour le bouton nouvelle partie
-	#if yes
-	#start()
-	#if no
-	#exit()
+	answer = tkinter.messagebox.askquestion("Nouvelle partie ?","Voulez vous faire une nouvelle partie ?")
+	if answer == "yes":
+		start()
+	else:
+		exit()
 
 def verifNombre(nb):
 	"vérifie si le nombre est un nombre"
@@ -62,14 +70,11 @@ def returnKey(event):
 	jeu()
 
 def jeu():
-	global nbTours, nbEssai
+	global nbTours, nbEssai, listNb
 	
-	if nbTours == 0:
-		can1.create_rectangle(50, 50, 200, 200, fill="red")
-		can1.itemconfig(txtJeu, text="Vous avez perdu ! :(")
-		nouvellePartie()
 
 	nombre = ent1.get()
+	ent1.delete(0, END)
 	nbEssai.append(nombre)
 
 	if len(nombre) == 0:
@@ -81,6 +86,18 @@ def jeu():
 		can1.itemconfig(txt1, text="Vous n'avez pas choisis un nombre.")
 
 	nbTours -= 1
+	can1.itemconfig(txtTour, text="Nombre de tours restants : {}".format(nbTours))
+	
+	nbEssai.sort()
+	for i in nbEssai:
+		listNb += " " + i
+
+	can1.itemconfig(txtEssai, text="Vous avez déjà essayé : {}".format(listNb))
+	
+	if nbTours == 0:
+		can1.itemconfig(recJeu, fill="red")
+		can1.itemconfig(txtJeu, text="Vous avez\n perdu ! :(")
+		nouvellePartie()
 
 def comparaison(nb):
 	global nbChoix
@@ -89,7 +106,7 @@ def comparaison(nb):
 
 	if nbComp == nbChoix:
 		can1.itemconfig(recJeu, fill="green")
-		can1.itemconfig(txtJeu, text="Vous avez trouvé le nombre {} !".format(nbChoix))
+		can1.itemconfig(txtJeu, text="Vous avez trouvé\n le nombre {} !".format(nbChoix))
 	elif nbComp < nbChoix:
 		can1.itemconfig(recJeu, fill="orange")
 		can1.itemconfig(txtJeu, text="Le nombre est\n plus grand.")
@@ -97,6 +114,21 @@ def comparaison(nb):
 		can1.itemconfig(recJeu, fill="orange")
 		can1.itemconfig(txtJeu, text="Le nombre est\n plus petit.")
 
+def flecheHaut():
+	"une fleche avec un rectangle et un triangle filled black"
+	pass
+
+def flecheBas():
+	"une fleche avec un rectangle et un triangle filled black"
+	pass
+
+def centrer():
+	fenetre.update_idletasks()
+	width = fenetre.winfo_width()
+	height = fenetre.winfo_height()
+	x = fenetre.winfo_screenwidth() // 2 - width // 2
+	y = fenetre.winfo_screenheight() // 2 - height // 2
+	fenetre.geometry("{}x{}+{}+{}".format(width, height, x, y))
 
 #programme
 
@@ -107,9 +139,10 @@ fenetre.resizable(0,0)
 #mise en place du canvas
 can1 = Canvas(fenetre, width=200, height=100, bg="light grey")
 txt1 = can1.create_text(100, 10, text="Hello, World!")
-txtTour = can1.create_text(100, 25, text="Nombre de tours restants : {}".format(nbTours))
-recJeu = can1.create_rectangle(50, 50, 150, 95, fill="light grey")
+txtTour = can1.create_text(100, 25, text="Nombre de tours restants : {}".format(nbTours), font="Arial 10 bold")
+recJeu = can1.create_rectangle(55, 55, 150, 95, fill="light grey", outline="light grey")
 txtJeu = can1.create_text(100, 75, text="")
+txtEssai = can1.create_text(100, 40, text="")
 
 #mise en place des inputs
 ent1 = Entry(fenetre, width=15)
@@ -130,4 +163,5 @@ start()
 #définition d'un bouton qui apparait seulement quand nbTours est à 0
 bouNP = Button(fenetre, text="Nouvelle Partie", command=start)
 
+centrer()
 fenetre.mainloop() #boucle principale
