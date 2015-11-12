32292a439a9f9b5605eb1f976d77c764c4fcd4fd
Michaël Seyne <michaelseyne@gmail.com>
2015-11-12 13:06:23 +0100

    + start 07

--- 07_solitaire.py
+++ 07_solitaire.py
@@ -1,1 +1,21 @@
 # -*- coding:Utf-8 -*-
+"Text based solitaire game using a grid, row/col, to place number"
+
+"""
+- (instead of classical playing card, I just use numbers and letters)
+- four cells to fill, A, B, C, D*
+- one stack of hiden cards and one cell for the card we check
+- seven cascades, with each, hidden cards
+- 13 * 4 = 52 cards (Ex: 1A, 2A, 12C, 13D, 11A, 9B, etc)
+"""
+
+#libraries
+
+#variable
+
+#functions
+def cardNames():
+	"une fonction pour créer les nom des 52 cartes"
+	pass
+
+#program
