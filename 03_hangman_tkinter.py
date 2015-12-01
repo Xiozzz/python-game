0289bd1d444d71bd3b59ab4fe1ab54c23e5e88bb
Michaël Seyne <michaelseyne@gmail.com>
2015-12-01 20:45:22 +0100

    wordBank

--- 03_hangman_tkinter.py
+++ 03_hangman_tkinter.py
@@ -4,6 +4,8 @@
 '''
 - pixel art custom gif format
 - start game, make vanish title and start button, make appear drawing, score, letter found, letter asked, user entry input, button ok
+- datas in text file (open, read, w, r, readline, readlines, etc) for bank words
+- menu, add or remove word in the wordbank
 '''
 #libraries
 from tkinter import *
@@ -11,17 +13,77 @@ from sys import exit
 
 #datas
 TITLE = "Hangman"
+
 WIDTH, HEIGHT = 400, 400
-COLORS = ["lemon chiffon", "dark salmon", "light blue"]
+
+COLORS = ["lemon chiffon", "dark salmon", "light blue", "chocolate"]
+
+INFO = [
+"Welcome, what do you want to do ?",
+"1. Start a new game",
+"2. Add or remove words in the bank.",
+"3. Quit the game"
+]
+
+wordBank = "wordbank.txt"
+
 #functions
+
+def menu():
+	"game menu, check, remove, add word in bank words or play new game"
+	gameTitle = gameScreen.create_text(WIDTH/2, HEIGHT/6, text=TITLE, fill=COLORS[1], font=("sans-serif", 20))
+	menuInfo1 = gameScreen.create_text(WIDTH/2, HEIGHT/2, text=INFO[0], fill=COLORS[3], font=("sans-serif", 12))
+	menuInfo2 = gameScreen.create_text(WIDTH/2, HEIGHT/2+30, text=INFO[1], fill=COLORS[3], font=("sans-serif", 10))
+	menuInfo3 = gameScreen.create_text(WIDTH/2, HEIGHT/2+60, text=INFO[2], fill=COLORS[3], font=("sans-serif", 10))
+	menuInfo4 = gameScreen.create_text(WIDTH/2, HEIGHT/2+90, text=INFO[3], fill=COLORS[3], font=("sans-serif", 10))
+
+	#define menu buttons, start new game, bank access, exit game
+	butStart = Button(gameInput, text="1. Start a New Game", command=newGame, height=2, width=20)
+	butBank = Button(gameInput, text="2. Word Bank Access", command=wordBank, height=2, width=20)
+	butQuit = Button(gameInput, text="Quit", command=exitRoot, height=2, width=8)
+	gridMenuSetup(butStart, butBank, butQuit)
+
 def newGame():
 	print("Hello, World!")
+	removeWidgets()
+	#remove menu button
+	#define game entry input
+	#define game button ok validation
 
-def gridSetupStart():
+def wordBank():
+	"word bank, to check, add and remove words"
+	print("Time to go to the bank")
+	removeWidgets()
+	#check the file wordbanb.txt and display all the words
+
+def addWord(word):
+	"menu"
+	words = open(wordBank, "a")
+	words.write(word)
+	words.close()
+
+
+def gridBankSetup():
+	pass
+
+def gridGameSetup():
+	pass
+
+def gridMenuSetup(butStart, butBank, butQuit):
 	"set the grid of the main program"
 	gameScreen.grid(row="1", column="1", columnspan="3", padx=0, pady=5)
 	gameInput.grid(row="2", column="1", columnspan="3", padx=10, pady=5)
-	butStart.grid(row='1', column="2", padx=(WIDTH//2)-75, pady=10)
+	butStart.grid(row='1', column="1", padx=5, pady=10)
+	butBank.grid(row='1', column="2", padx=5, pady=10)
+	butQuit.grid(row='1', column="3", padx=5, pady=10)
+
+def removeWidgets():
+	"remove the widget in the main canva and frame"
+
+	for widget in gameInput.winfo_children():
+		widget.destroy()
+
+	gameScreen.delete(ALL)
 
 def windowSetup():
 	"center in screen the window"
@@ -46,14 +108,6 @@ if __name__ == "__main__":
 	gameScreen = Canvas(root, width=WIDTH, height=HEIGHT, bg=COLORS[0])
 	gameInput = Frame(root, bg=COLORS[2])
 
-	#define game 
-	gameTitle = gameScreen.create_text(WIDTH/2, HEIGHT/2, text=TITLE, fill=COLORS[1], font=("sans-serif", 20))
-
-	#define game entry
-
-	#define game buttons, start new game, ok validation inpu, exit game
-	butStart = Button(gameInput, text="Start a New Game", command=newGame, height=2, width=20)
-
-	gridSetupStart()
+	menu()
 	windowSetup() 
 	root.mainloop()
