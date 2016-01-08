5c5c9fd1f7a73c1adca9525230c9ca1f1b77ea98
mseyne <michaelseyne@gmail.com>
2016-01-08 14:51:33 +0100

    paddle

--- pong.py
+++ pong.py
@@ -15,47 +15,82 @@ from sys import exit
 #datas
 
 TITLE = "Pong with Tkinter"
-COLORS = ["lemon chiffon", "dark salmon", "light blue", "chocolate", "firebrick", "ivory"]
-WIDTH, HEIGHT = 500, 300
+COLORS = ["cornflower blue", "dark salmon", "royal blue", "chocolate", "firebrick", "ivory", "gainsboro"]
+WIDTH, HEIGHT = 550, 350
+TURNS = ["PLAYER", "COMPUTER"]
 
 #variables
 
 score = [0, 0]
+gameFlag = 0
 
 #class
 
 class Paddle:
 	''' paddle object '''
 
-	def __init__(self, x, y):
-		self.xcord = x
-		self.ycord = y
+	def __init__(self, x, y, color):
+		self.paddle = gameScrn.create_rectangle(x-10, y-40, x+10, y+40, fill=color, width=0)
+		self.xcoord = x
+		self.ycoord = y
+
+	def moveUp(self):
+		if gameFlag == 1:
+			self.ycoord -= 5
+			gameScrn.coords(self.paddle, self.xcoord-10, self.ycoord-40, self.xcoord+10, self.ycoord+40)
+
+	def moveDown(self):
+		if gameFlag == 1:
+			self.ycoord += 5
+			gameScrn.coords(self.paddle, self.xcoord-10, self.ycoord-40, self.xcoord+10, self.ycoord+40)
 
 class Ball:
 	''' ball object '''
 
 	def __init__(self, x, y):
+		self.ball = gameScrn.create_oval(x-10, y-10, x+10, y+10, fill=COLORS[1], width=0)
 		self.xcord = x
 		self.ycord = y
 
+	def movement(self):
+		if gameFlag == 0:
+			#move
+			pass
+
 #functions
-
-
-#Game logic
 
 def newGame():
 	"reset to default variables and launch a new game"
+	global gameFlag
 	print("New Game !! Yes Pong !!")
+	gameFlag = 1
 	pass
+
+def pauseGame():
+	"pause the game"
+	gameFlag = 0
+
+def stopGame():
+	"stop game and return to menu"
+	gameFlag = 0
 
 #Game display
 def displayBoardgame():
 	"display background"
-	pass
+	gameScrn.create_line(WIDTH/2, 10, WIDTH/2, HEIGHT-10, fill=COLORS[6], width=2) #center
+	gameScrn.create_line(10, 10, WIDTH-10, 10, fill=COLORS[6], width=2) #up side
+	gameScrn.create_line(10, HEIGHT-10, WIDTH-10, HEIGHT-10, fill=COLORS[6], width=2) #down side
+	gameScrn.create_line(10, 10, 10, HEIGHT-10, fill=COLORS[6], width=2) #left side
+	gameScrn.create_line(WIDTH-10, 10, WIDTH-10, HEIGHT-10, fill=COLORS[6], width=2) #right side
 
-def updateScore():
-	"display score in background"
-	pass
+def displayScore():
+	"score in background"
+	#player score
+	gameScrn.create_text(WIDTH/4*3, 20, text=TURNS[0], fill=COLORS[0], font=("Helvetica", 12))
+	gameScrn.create_text(WIDTH/4*3, 35, text=score[0], fill=COLORS[0], font=("Helvetica", 12))
+	#computer score
+	gameScrn.create_text(WIDTH/4, 20, text=TURNS[1], fill=COLORS[3], font=("Helvetica", 12))
+	gameScrn.create_text(WIDTH/4, 35, text=score[1], fill=COLORS[3], font=("Helvetica", 12))
 
 #Window parameters
 def configWindow():
@@ -70,7 +105,6 @@ def configWindow():
 	y = root.winfo_screenheight()//2 - height//2
 	root.geometry("{}x{}+{}+{}".format(width, height, x, y))
 	
-
 def setGameGrid():
 	"display main grid of the program"
 	gameScrn.grid(row=1, column=1, padx=0, pady=0)
@@ -87,11 +121,27 @@ def exitWindow():
 if __name__ == "__main__":
 	root = Tk()
 
-	gameScrn = Canvas(root, width = WIDTH, height = HEIGHT, bg=COLORS[5])
+	gameScrn = Canvas(root, width = WIDTH, height = HEIGHT, bg=COLORS[5], highlightthickness=0)
 	gameMenu = Frame(root, width= WIDTH, height=HEIGHT/4)
 	
 	butST = Button(gameMenu, text="New Game", command=newGame)
 	butQT = Button(gameMenu, text="Quit", command=exitWindow)
+
+	# draw background and score
+	displayBoardgame()
+	displayScore()
+
+	# create paddles
+	playerPad = Paddle(WIDTH-20, HEIGHT/2, COLORS[2])
+	computerPad = Paddle(20, HEIGHT/2, COLORS[4])
+
+	# create ball
+	gameBall = Ball(WIDTH/2, HEIGHT/2)
+
+	# bind up and down to the game screen
+	gameScrn.bind("<Down>", lambda e: playerPad.moveDown())
+	gameScrn.bind("<Up>", lambda e: playerPad.moveUp())
+	gameScrn.focus_set()
 
 	setGameGrid()
 	configWindow()
